b2300f06958c21c842d6db0fc49ac0e232b6b8d4
CianMRoche <croche1@nd.edu>
2023-03-23 03:05:56 -0400

    hacky multithread, updated line example, plotting

--- src/plot_chains.py
+++ src/plot_chains.py
@@ -4,29 +4,29 @@
 # Quick python program to plot chains produced by the
 # save_chain() function in MC Julia.
 
-from numpy import loadtxt, log
-from matplotlib import pyplot as plot
+from numpy import loadtxt, log, load
+import matplotlib.pyplot as plt
 from sys import argv, exit
 from os.path import exists
 
 if len(argv) < 2:
-    print "Error: no filename given."
-    print "Usage:   %s <filename>" % (argv[0])
+    print("Error: no filename given.")
+    print("Usage:   %s <filename>" % (argv[0]))
     exit()
 if len(argv) > 2:
-    print "Warning: too many filenames."
-    print "The following were ignored:  %s" % " ".join(argv[2:])
+    print("Warning: too many filenames.")
+    print("The following were ignored:  %s" % " ".join(argv[2:]))
 
 filename = argv[1]
 
 if not exists(filename):
-    print "Error: file not found: %s" % filename
+    print("Error: file not found: %s" % filename)
     exit()
 
 try:
-    chain = loadtxt(filename, delimiter=",")
+    chain = load(filename)#, delimiter=",")
 except ValueError:
-    print "Error: could not read file %s." % filename
+    print("Error: could not read file %s." % filename)
     exit()
 
 s = chain.shape
@@ -37,10 +37,10 @@ else:
     steps = s[0]
     dim = 1
 
-plot.figure(0)
+plt.figure(0)
 
 # Loop over the chains for each parameter
-for i in xrange(dim):
+for i in range(dim):
     if dim > 1:
         data = chain[i,:]
     else:
@@ -48,30 +48,30 @@ for i in xrange(dim):
     mean = data.mean()
     std = data.std()
     # Plot chain
-    plot.figure(0)
+    plt.figure(0)
     sub = dim*100 + 10 + i+1
-    plot.subplot(sub)
-    plot.plot(data, color="black")
+    plt.subplot(sub)
+    plt.plot(data, color="black")
     
     # Plot histogram
-    plot.figure(i+1)
-    counts, bins, patches = plot.hist(data, 30, color="black")
+    plt.figure(i+1)
+    counts, bins, patches = plt.hist(data, 30, color="black")
     mode = bins[counts.argmax()] + (bins[1]-bins[0])/2.0
     hist_title = u"Parameter #%d: mode = %.2f, mean±std = %.3f ± %.3f"
-    plot.title(hist_title % (i+1, mode, mean, std))
-  #  plot.axvline(mean, color="red")
- #   plot.axvline(mean+std, linestyle="--", color="red")
-#    plot.axvline(mean-std, linestyle="--", color="red")
+    plt.title(hist_title % (i+1, mode, mean, std))
+  #  plt.axvline(mean, color="red")
+ #   plt.axvline(mean+std, linestyle="--", color="red")
+#    plt.axvline(mean-std, linestyle="--", color="red")
 
 
 # Make correlation plot
 if dim <= 9 and dim > 1:
-    plot.figure(dim+1)
-    for i in xrange(0, dim-1):
-        for j in xrange(0, dim):
+    plt.figure(dim+1)
+    for i in range(0, dim-1):
+        for j in range(0, dim):
             if i < j:
-                plot.subplot(dim-1, dim-1, i*(dim-1) + j)
-                plot.ylabel("Parameter #%d" % (i+1))
-                plot.xlabel("Parameter #%d" % (j+1))
-                plot.scatter(chain[j,:], chain[i,:], 0.1, color="black")
-plot.show()
+                plt.subplot(dim-1, dim-1, i*(dim-1) + j)
+                plt.ylabel("Parameter #%d" % (i+1))
+                plt.xlabel("Parameter #%d" % (j+1))
+                plt.scatter(chain[j,:], chain[i,:], 0.1, color="black")
+plt.show()
